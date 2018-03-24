37e8903f6925a3c14b2aee5a2ec93361a5e4949d
pro11120 <36438528+pro11120@users.noreply.github.com>
2018-03-24 14:44:04 +0700

    Update javeb_bot.php

--- javeb_bot.php
+++ javeb_bot.php
@@ -30,7 +30,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดีค่ะ"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
+  $arrPostData['messages'][0]['text'] = "";
 }
  
  
