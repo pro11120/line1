04457129872028be5f1f29a8c8078245b8aec587
pro11120 <36438528+pro11120@users.noreply.github.com>
2018-03-24 13:30:44 +0700

    Update javeb_bot.php

--- javeb_bot.php
+++ javeb_bot.php
@@ -15,7 +15,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดีค่ะ"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "สวัสดีค่ะ";
+  $arrPostData['messages'][0]['text'] = "";
 }else if($arrJson['events'][0]['message']['text'] == "ลงGEผิดจะแก้ยังไง"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
