b256ca9b557c691988206cdb51534e1652395c53
pro11120 <36438528+pro11120@users.noreply.github.com>
2018-03-24 13:25:34 +0700

    Update javeb_bot.php

--- javeb_bot.php
+++ javeb_bot.php
@@ -1,6 +1,6 @@
 <?php
  
-$strAccessToken = "j9c3KJKt2+3aoVnyTtp1yJm9lnpzsuRb+nz87EkXGY48+5r8KX/8LcUmj3ZRDXfgz7GqXnp7//BYAWTdnOmNmLigr8y/Utq3/zurwITjzPX/X5M7JYtr7ePEsT0+bRe887TccsbZOOZ+DoK7IaQFxQdB04t89/1O/w1cDnyilFU=";
+$strAccessToken = "vSgDomCOuIUA4kwWL4DWy1eqp2vZDmQ6IMj0/EDRIfQ43wtE41+n1SWH6mTTrqzlJgWt87p1r0xDBt6tctVhAZI9/twiQL/Xb6wbYKxfrhU8KMkPrgzZz4b72fzobyyMT4n8p1XKy4dGxqzQWZ9xKwdB04t89/1O/w1cDnyilFU=";
  
 $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
